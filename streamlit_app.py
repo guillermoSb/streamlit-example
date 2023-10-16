715bacb50e051dbd51186300f309da57e077aa81
Guillermo Santos <34350127+guillermoSb@users.noreply.github.com>
2023-10-16 22:13:51 +0000

    Pagina de home

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+
 from collections import namedtuple
 import altair as alt
 import math
@@ -8,5 +9,15 @@ import plotly.express as px
 
 """
 # Universidad del Valle de Guatemala
+## Laboratorio 09
 """
 
+
+"""
+Este laboratorio utiliza los datos del laboratorio 02 para crear un dashboard interactivo.
+El set de datos incluye:
+- Importaciones de gasolina
+- Precios de gasolina
+"""
+
+st.info('Utilizar el menu de la izquierda para navegar entre las páginas', icon="ℹ️")
